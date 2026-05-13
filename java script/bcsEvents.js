81e1e06554a3ed411f946fefe3c909ce0393da95
Rafikun Nesa Hena <rafikun2523@gmail.com>
2026-05-14 00:32:08 +0600

    Update bcsEvents.js

--- java script/bcsEvents.js
+++ java script/bcsEvents.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const BASE_URL = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : `http://${window.location.hostname}:5000`;
+    : ``;
 
   function showToast(msg, isError = false) {
     const toast = document.createElement("div");
