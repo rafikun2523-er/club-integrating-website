626dc86de13fbbb239b00878a3d16ef60efa35cd
Rafikun Nesa Hena <rafikun2523@gmail.com>
2026-05-14 00:28:46 +0600

    Update login.js

--- java script/login.js
+++ java script/login.js
@@ -1,7 +1,7 @@
 const BASE_URL = window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
   ? "http://localhost:5000"
-  : `http://${window.location.hostname}:5000`;
+  : ``;
 
 document.querySelectorAll(".toggle-eye").forEach(eye => {
   eye.addEventListener("click", () => {
