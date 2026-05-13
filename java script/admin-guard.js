260137afda451f5f8d62d4e84c6b21632d842a1c
Rafikun Nesa Hena <rafikun2523@gmail.com>
2026-05-14 00:33:40 +0600

    Update admin-guard.js

--- java script/admin-guard.js
+++ java script/admin-guard.js
@@ -1,7 +1,7 @@
 const BASE_URL = window.location.hostname === "localhost" ||
                  window.location.hostname === "127.0.0.1"
   ? "http://localhost:5000"
-  : `http://${window.location.hostname}:5000`;
+  : ``;
 
 function getAdminToken() {
   return localStorage.getItem("adminToken");
