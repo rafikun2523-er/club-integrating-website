90724911c6a4c8e4562f8f4042cafaf7fc37aaf8
Rafikun Nesa Hena <rafikun2523@gmail.com>
2026-05-14 00:29:05 +0600

    Update events.js

--- java script/events.js
+++ java script/events.js
@@ -1,7 +1,7 @@
 const BASE_URL = window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
   ? "http://localhost:5000"
-  : `http://${window.location.hostname}:5000`;
+  : ``;
 
 let allEventsData = [];
 const COLORS = ["#2B2E83", "#1a5276", "#a93226", "#1e6b3c", "#6c3483", "#854F0B"];
