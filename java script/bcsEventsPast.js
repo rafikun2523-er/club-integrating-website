ed417c927141f769cd31d4ea6c1a417858be4fae
Rafikun Nesa Hena <rafikun2523@gmail.com>
2026-05-14 00:31:49 +0600

    Update bcsEventsPast.js

--- java script/bcsEventsPast.js
+++ java script/bcsEventsPast.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const BASE_URL = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : `http://${window.location.hostname}:5000`;
+    : ``;
 
   async function loadPastEvents() {
     const container = document.getElementById("eventsContainer");
