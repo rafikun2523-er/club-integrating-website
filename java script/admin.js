2a1af21310886659a6c67b88e59756b5540f8643
Rafikun Nesa Hena <rafikun2523@gmail.com>
2026-05-14 00:33:25 +0600

    Update admin.js

--- java script/admin.js
+++ java script/admin.js
@@ -1,7 +1,7 @@
 const BASE_URL_ADMIN = window.location.hostname === "localhost" ||
                        window.location.hostname === "127.0.0.1"
   ? "http://localhost:5000"
-  : `http://${window.location.hostname}:5000`;
+  : ``;
 
 let allStudents = [];
 let allEvents   = [];
