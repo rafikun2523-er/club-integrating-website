99641d994645d1114f8d57afc42a6cc7c9d923e4
Rafikun Nesa Hena <rafikun2523@gmail.com>
2026-05-14 00:30:10 +0600

    Update clubs.js

--- java script/clubs.js
+++ java script/clubs.js
@@ -3,7 +3,7 @@ const BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : `http://${window.location.hostname}:5000`;
+    : ``;
 
 
 const clubs = [
